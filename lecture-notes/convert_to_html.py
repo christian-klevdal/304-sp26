9a6411db7aef06a81f2f3d7d7499b068562d866d
christian-klevdal <christian.klevdal@gmail.com>
2026-01-26 14:23:19 -0500

    Fix MathJax inline math rendering by moving configuration before script load

--- lecture-notes/convert_to_html.py
+++ lecture-notes/convert_to_html.py
@@ -156,15 +156,15 @@ def convert_notebook_to_html(notebook_path, output_path=None):
     
     <!-- MathJax for TeX rendering -->
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
     <script>
-        MathJax = {{
-            tex: {{
+        window.MathJax = {
+            tex: {
                 inlineMath: [['$', '$'], ['\\\\(', '\\\\)']],
                 displayMath: [['$$', '$$'], ['\\\\[', '\\\\]']]
-            }}
-        }};
+            }
+        };
     </script>
+    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
     
     <style>
         .markdown-cell {{
